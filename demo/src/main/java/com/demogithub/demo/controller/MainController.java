d68a6a6a66c9b4f84b44b50904fdcdc72776cc52
prashantG4 <occs_admin@mahadiscom.in>
2025-05-25 00:05:54 +0530

    main controller

---
+++ demo/src/main/java/com/demogithub/demo/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.demogithub.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Hi, How are you?";
+	}
+
+}
